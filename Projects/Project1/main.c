59d5da7a668c9a956701c991826a18794fa2f5b3
unknown <zdw7287@1640-10.se.rit.edu>
2017-02-01 20:37:30 -0500

    Project1 codebase - POST function idea

---
+++ Projects/Project1/main.c
@@ -0,0 +1,52 @@
+#include "stm32l476xx.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int POST(unsigned char *PIN); //prototype for Power On Self Test
+int main(void){
+	//POST - Power on self test
+	//GPIO sees at leasr 1 pulse ever 100ms
+	//If fails, option to POST again - pass then move to UI
+	
+	//UI
+	//Display lower and upper limits
+	//-> 950 micro to 1050 micro
+	//Prompt user for acceptance or new limits
+	//CONSTRAINT! Upper limit must be 100 micro above lower
+	//Lower can range from 50 micro to 9950 micro
+	//Start prompt and wait for enter to begin
+	//Take 1000 measurements(1001 rising edges)
+	//DNon-zero histogram count in ascending on vTerminal
+	//Dont display 
+	
+	unsigned char *GPIOA_ptr = (unsigned char *) 0x48000000; //GPIO pin A
+	int pass;
+	pass=POST(GPIOA_ptr); //POST function on GPIO pin A
+	if (pass==0){
+		return(0);
+	}
+}
+
+int POST(unsigned char *PIN){
+	//80MHZ clk - .0000000125 sec poll
+	int counter=0;
+	char input;
+	while(counter<8000000){ // (.100/.0000000125)
+		if(*PIN==1){ //pulse see
+			break; //add functionality to see the resulkt of when pulse was seen
+			return(1);
+		}
+		else{
+			printf("POST failed! Pulse not seen in 100ms. Rerun? (Y or N): ");
+			scanf("%c",&input);
+			if (input == 'Y'||input=='y'){
+				POST(PIN);
+				return 0;
+			}
+			else{ //input wasn't y or Y
+				return 0;
+			}
+		}
+	}
+}
