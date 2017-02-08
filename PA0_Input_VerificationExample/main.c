7cec0c2a7b1a9730a20c6ce25bc5228630356f45
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-02-08 17:46:37 -0500

    Added new GPIO setup and input demo example - needs to be switched to AF mode to connect to timer capture input

---
+++ PA0_Input_VerificationExample/main.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "stm32l476xx.h"
+#include "SysClock.h"
+#include "LED.h"
+#include "input_pa0_test.h"
+
+int main(void)
+{
+	
+	System_Clock_Init(); // Switch System Clock = 80 MHz
+	LED_Init();
+	init_pa0() ;
+	monitor_pa0() ;		// loops forever!	
+}
+
