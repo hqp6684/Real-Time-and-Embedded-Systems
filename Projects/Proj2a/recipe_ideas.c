fec28f87cf7ef931298d4f73f583cb5e1ff1c2b3
Zack <zackweeden@aol.com>
2017-02-17 11:45:00 -0500

    Modified document and example recipes

---
+++ Projects/Proj2a/recipe_ideas.c
@@ -0,0 +1,41 @@
+// L. Kiser Feb. 16, 2017
+// Some ideas for writing the SWEN 563 recipe related code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define all of the commands that are valid
+#define MOV (0x20)
+#define RECIPE_END (0)
+
+// Examples of simple recipes
+// Note that, because the bottom 5 bits are zeros adding or bitwise or'ing
+// in the values for the bottom 5 bits are equivalent. However, using a bitwise
+// or is better at communicating your purpose.
+unsigned char recipe1[] = { MOV + 3, MOV | 5, RECIPE_END } ;
+unsigned char recipe2[] = { MOV | 5, MOV | 2, RECIPE_END } ;
+
+// If you set up an array like this then you can easily switch recipes
+// using an additional user input command.
+unsigned char *recipes[] = { recipe1, recipe2, NULL } ;
+
+// This is a good way to define the states of a state machine.
+enum status 
+	{
+		running,
+		paused,
+		command_error,
+		nested_error 
+	} ;
+
+// A simple main that just prints out the hex value of the first entry in each recipe.
+void main()
+{
+	int i = 0 ;
+
+	while ( recipes[ i ] )
+	{
+		printf( "%02x\n", *recipes[ i++ ] ) ;	// printf first hex entry of each recipe
+
+	}
+}
