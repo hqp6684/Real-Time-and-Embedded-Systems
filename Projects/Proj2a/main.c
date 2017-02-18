4ccfc624e620dee984d7848e5356212c22a496fa
Zack <zackweeden@aol.com>
2017-02-18 17:38:03 -0500

    Proj2 start - input_pa0 complete

---
+++ Projects/Proj2a/main.c
@@ -0,0 +1,10 @@
+// Z. Weeden Feb. 18, 2017
+#include "stm32l476xx.h" // Board
+#include "SysClock.h"    // Sysclock
+#include "LED.h"         // LED
+#include "UART.h"        // UART
+#include "TIMER.h"       // TIMER
+#include "input_pa0_test.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
