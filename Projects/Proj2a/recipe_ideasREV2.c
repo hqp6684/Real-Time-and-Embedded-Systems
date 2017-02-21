0d8f5046128ec624e8eabfe348ddbd482b3318ca
Zack <zackweeden@aol.com>
2017-02-20 19:03:03 -0500

    Modified recipe example and added rev2 from professor

--- Projects/Proj2a/recipe_ideasREV2.c
+++ Projects/Proj2a/recipe_ideasREV2.c
@@ -1,25 +1,18 @@
 // L. Kiser Feb. 16, 2017
-// Contributor: Zachary Weeden
-// Some ideas for writing the SWEN 563 recipe related code. - 200 ms per position 
+// Some ideas for writing the SWEN 563 recipe related code.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 // Define all of the commands that are valid
-#define MOV (0x20)		//001x xxxx
-#define WAIT (0x40)		//010x xxxx
-#define LOOP (0x80)		//100x xxxx
-#define END_LOOP (0xA0)	//101x xxxx
-
+#define MOV (0x20)
 #define RECIPE_END (0)
 
 // Examples of simple recipes
 // Note that, because the bottom 5 bits are zeros adding or bitwise or'ing
 // in the values for the bottom 5 bits are equivalent. However, using a bitwise
 // or is better at communicating your purpose.
-//							0010 0011, 0010 0101
 unsigned char recipe1[] = { MOV + 3, MOV | 5, RECIPE_END } ;
-//						    0010 0101, 0010 0010
 unsigned char recipe2[] = { MOV | 5, MOV | 2, RECIPE_END } ;
 
 // If you set up an array like this then you can easily switch recipes
@@ -27,6 +20,7 @@ unsigned char recipe2[] = { MOV | 5, MOV | 2, RECIPE_END } ;
 unsigned char *recipes[] = { recipe1, recipe2, NULL } ;
 
 // This is a good way to define the status of the display.
+// This should be in a header (.h) file.
 enum status 
 {
 	status_running,
@@ -92,7 +86,7 @@ void main()
 
 	while ( recipes[ i ] )
 	{
-		printf( "%02x %02x\n", *(recipes[ i ]), *(recipes[ i ] + 1)) ;	// printf hex entries of each recipe
-		i++;
+		printf( "%02x\n", *recipes[ i++ ] ) ;	// printf first hex entry of each recipe
+
 	}
 }
