b7f8b124e78644cf3e4fe06403659ec5bce9c348
Zack <zackweeden@aol.com>
2017-03-29 18:31:01 -0400

    Cleaning up spacing and added comments

--- Projects/Proj4/project4_main.c
+++ Projects/Proj4/project4_main.c
@@ -1,3 +1,4 @@
+// Z. Weeden Mar. 29, 2017
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,21 +6,22 @@
 #include <pthread.h>
 
 
-#define MIN_ARRIVAL (60) //1 minute
-#define MAX_ARRIVAL (240)//4 minutes
+#define MIN_ARRIVAL (60)        // 1 minute
+#define MAX_ARRIVAL (240)       // 4 minutes
+#define MIN_TRANSACTION (30)    // 30 seconds
+#define MAX_TRANSACTION (480)   // 8 minutes
 
-#define MIN_TRANSACTION (30) //30 seconds
-#define MAX_TRANSACTION (480)//8 minutes
+int arrivalTimeArray[];
+int transactionQueueArray[];
 
-int arrivalTimeArray[];		//Need this to be dynamic - keep appended customers to queue - initialize dont specify size
-int transactionQueueArray[];	//Need this to be dynamic - keep appended customers to queue - initialize dont specify size
-
-int randMToN(int M, int N){
-	return M + (rand() / ( RAND_MAX / (N+1-M) ) ) ;
+/* This generates a random number within range of the passed parameters inclusively, while overall 
+producing a uniform distribution of generated values. */
+int getRandomWithRange(int lower, int upper){
+    return lower + (rand() / (RAND_MAX / (upper + 1 - lower))) ;
 }
 
-//Pulled from my example of sleep prints see 'thread_dummy.c'
-//In each thread make sure to handle initial condition - while queue is empty do nothing
+// Pulled from my example of sleep prints see 'thread_dummy.c'
+// In each thread make sure to handle initial condition - while queue is empty do nothing
 void *tellerThread1(void *vargp){
     sleep(5);
     printf("From teller1 \n");
@@ -39,39 +41,39 @@ void *tellerThread3(void *vargp){
 }
 
 int main(void) {
-	int i=0;
-	int arrivalTime=0;
-	int transactionTime=0;
+    int i=0;
+    int arrivalTime=0;
+    int transactionTime=0;
 
-	//Thread Ids
+    // Thread Ids
     pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
 
-    //Creating threads
+    // Creating threads
     pthread_create(&tid1, NULL, tellerThread1, NULL);
     pthread_create(&tid2, NULL, tellerThread2, NULL);
     pthread_create(&tid3, NULL, tellerThread3, NULL);
 
-    //Joining to Pool 
+    // Joining to Pool 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
     pthread_join(tid3, NULL);
 
-	while(1){
-		arrivalTime=randMToN(MIN_ARRIVAL,MAX_ARRIVAL);					//generate random arrival time of customer
-		if (arrivalTime+currentTime)<closingTime{						//pseudo code for checking hours of operations condition
-			arrivalTimeArray[i]=arrivalTime;							//append the arrival time to the array because it is valid in within hours of operation
-			transactionTime=randMToN(MIN_TRANSACTION,MAX_TRANSACTION);	//generate random transaction time of customer
-			sleep(arrivalTimeArray[i]);									//dont append to queue until after sleep
-			transactionQueueArray[i]=transactionTime; 					//queue will be array of customer transaction times
-			i++;
-		}
-		else{
-			//Customer cant be seen because it is past hours - bank is closed
-			break;
-		}
-	}
-	report(arrivalTimeArray, transactionQueueArray); //parameterized report function here to display metrics
-	return EXIT_SUCCESS;
+    while(1){
+        arrivalTime = getRandomWithRange(MIN_ARRIVAL,MAX_ARRIVAL);                  // generate random arrival time of customer
+        if (arrivalTime + currentTime) < closingTime{                               // pseudo code for checking hours of operations condition
+            arrivalTimeArray[i] = arrivalTime;                                      // append the arrival time to the array because it is valid in within hours of operation
+            transactionTime = getRandomWithRange(MIN_TRANSACTION,MAX_TRANSACTION);  // generate random transaction time of customer
+            sleep(arrivalTimeArray[i]);                                             // dont append to queue until after sleep
+            transactionQueueArray[i] = transactionTime;                             // queue will be array of customer transaction times
+            i++;
+        }
+        else{
+            // Customer cant be seen because it is past hours - bank is closed
+            break;
+        }
+    }
+    report(arrivalTimeArray, transactionQueueArray); // parameterized report function here to display metrics
+    return EXIT_SUCCESS;
 }
