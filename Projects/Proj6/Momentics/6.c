dcf7332143368eb3b43c98404193c1eca8996d3c
Zack <zackweeden@aol.com>
2017-05-03 17:53:14 -0400

    Adding device register access logic

--- Projects/Proj6/Momentics/6.c
+++ Projects/Proj6/Momentics/6.c
@@ -1,3 +1,25 @@
+#include <sys/neutrino.h>
+#include <stdint.h>
+#include <sys/mman.h>
+
+#define IO_PORT_SIZE                (1)
+#define BASE_ADDRESS                (0x280) //QNX base address - a/d lsb
+#define A_D                         (BASE_ADDRESS+1) //
+#define A_D_CHANNEL                 (BASE_ADDRESS+2) //
+#define A_D_GAIN_STATUS             (BASE_ADDRESS+3) //
+#define INTRPT_DMA_CONTROL_COUNTER  (BASE_ADDRESS+4)
+#define FIFO_THRESHOLD              (BASE_ADDRESS+5)
+#define D_A_LSB                     (BASE_ADDRESS+6)
+#define D_A_MSB                     (BASE_ADDRESS+7)
+#define PORT_A_OUT                  (BASE_ADDRESS+8)
+#define PORT_B_OUT                  (BASE_ADDRESS+9)
+#define PORT_C_OUT                  (BASE_ADDRESS+0xA)
+#define DIRECTION_CONTROL           (BASE_ADDRESS+0xB) //
+#define COUNTER_TIMER_B0            (BASE_ADDRESS+0xC)
+#define COUNTER_TIMER_B1            (BASE_ADDRESS+0xD) //
+#define COUNTER_TIMER_B2            (BASE_ADDRESS+0xE)
+#define COUNTER_TIMER_CONTROL       (BASE_ADDRESS+0xF)
+
 /*
 
 0 voltage = position 3
@@ -36,4 +58,21 @@ void convertToDigitalAndOutput(void){
             ;
         }
     }
+}
+
+void main(void){
+    if ( ThreadCtl(_NTO_TCTL_IO, NULL) == -1){ // request access rights to the hardware I/O for the thread
+        perror("Failed to get I/O access permission");
+        return 1;
+    }
+
+    uintptr_t baseHandle;
+    baseHandle = mmap_device_io(IO_PORT_SIZE, BASE_ADDRESS);
+    if(baseHandle == MAP_DEVICE_FAILED){
+        perror("Failed to map base addr");
+        return 2;
+    }
+
+
+
 }
