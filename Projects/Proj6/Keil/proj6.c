2bfc0a47d80d84e257c566c8b49724a1d248f19a
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-05-11 19:25:01 -0400

    Ridding of a commented function

--- Projects/Proj6/Keil/proj6.c
+++ Projects/Proj6/Keil/proj6.c
@@ -14,55 +14,6 @@
 // position        0  1   2   3   4   5
 int pwm_vars[6] = {4, 7, 10, 13, 16, 21};
 
-/*
-
-0 voltage = position 3
-- voltage = positions 4 and 5
-+ voltage = position 0, 1, and 2 
-
-VOLTAGE   -5   -4   -3   -2   -1      0    1    2    3    4    5 
-POSITION   5           4              3       2        1       0
-CCRx       21          16            13       10       7       4  
-
-*/
-
-/*
-//function to be ran on STM
-void monitorADCVoltagePinUpdateCCR(void){
-    static int joystickButton = 0; //monitor josytick press
-    //joystickButton = register?;
-    while (1){
-        if (joystickButton == 1){
-            joystickButton = 1; //acts as init - doesnt need to be held high
-            voltage = fetchVOLTAGEFromPIN(); //continually update
-            if (voltage < 0 && voltage > -5){  //negative voltage leftmost position
-                RED_LED_OFF(); //valid voltage
-                CCRx = 21 //position5;
-            }
-            else if (voltage == 0){ 
-                RED_LED_OFF(); //valid voltage
-                CCRx = 13 //position3;
-            }
-            else if (voltage > 0 && voltage < 5){ //positive voltage rightmost position
-                RED_LED_OFF(); //valid voltage
-                CCRx = 4 //position0;
-            }
-            else if (voltage > 5){ //voltage has gone over accepted value
-                RED_LED_ON(); //indicate failure on STM/servo
-                //printf("Voltage has gone over +5V\n"); //indicate failure in momentics/qnx
-            }
-            else if (voltage < -5){ //voltage has gone lower accepted value
-                RED_LED_ON(); //indicate failure on STM/servo
-                //printf("Voltage has gone under -5V\n"); //indicate failure in momentics/qnx
-            }
-            else{
-                ;
-            }
-        }
-    }
-}
-*/
-
 /* pseudo range for resolving ccr
 if (input_voltage >= -5.0 && input_voltage < -3.75){
     RED_LED_OFF();
