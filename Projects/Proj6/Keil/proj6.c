c269e3cd4911e2fade1864595b144715d2416062
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-05-12 00:21:10 -0400

    Should remedy jerky position-to-position motion with scaling function

--- Projects/Proj6/Keil/proj6.c
+++ Projects/Proj6/Keil/proj6.c
@@ -9,41 +9,23 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 // voltage         +5V        0V    -5V
 // position        0  1   2   3   4   5
 int pwm_vars[6] = {4, 7, 10, 13, 16, 21};
 
 /* pseudo range for resolving ccr
-if (input_voltage >= -5.0 && input_voltage < -3.33){
-    RED_LED_OFF();
-    TIM2->CCR1 = 21;
-}
-else if (input_voltage >= -3.33 && input_voltage < -1.67){
-    RED_LED_OFF();
-    TIM2->CCR1 = 16;
-}
-else if (input_voltage >= -1.67 && input_voltage < 1.25){
-    RED_LED_OFF();
-    TIM2->CCR1 = 13;
-} 
-else if (input_voltage >= 1.25 && input_voltage < 2.5){
-    RED_LED_OFF();
-    TIM2->CCR1 = 10;
-}
-else if (input_voltage >= 2.5 && input_voltage < 3.75){
-    RED_LED_OFF();
-    TIM2->CCR1 = 7;
-}
-else if (input_voltage >= 3.75 && input_voltage <= 5.0){
-    RED_LED_OFF();
-    TIM2->CCR1 = 4;
-}
-else{
+
+if (input_voltage < -5.0 || input_voltage > 5.0){
     RED_LED_ON();
     USART_Write(USART2, (uint8_t *)"\r\nVoltage out of range.\r\n", 25);
 }
-
+else{
+    RED_LED_OFF();
+    double fine_ccr = (-1.70*input_voltage)+12.727;
+    int rounded_ccr = round(fine_ccr);
+    TIM2->CCR1 = rounded_ccr;
+}
 */
 int main(void){
     System_Clock_Init();
