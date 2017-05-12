d120ab4543d9ed396e890fd09a07ebc9f7c6b02d
Zachary Weeden <zweed4u@users.noreply.github.com>
2017-05-12 00:50:53 -0400

    Adjusting commented stm code

--- Projects/Proj6/Keil/proj6.c
+++ Projects/Proj6/Keil/proj6.c
@@ -10,38 +10,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// voltage         +5V        0V    -5V
-// position        0  1   2   3   4   5
-int pwm_vars[6] = {4, 7, 10, 13, 16, 21};
 
-/* pseudo range for resolving ccr
-
-if (input_voltage < -5.0 || input_voltage > 5.0){
-    RED_LED_ON();
-    USART_Write(USART2, (uint8_t *)"\r\nVoltage out of range.\r\n", 25);
-}
-else{
-    RED_LED_OFF();
-    double fine_ccr = (-1.70*input_voltage)+12.727;
-    int rounded_ccr = round(fine_ccr);
-    TIM2->CCR1 = rounded_ccr;
-}
-*/
 int main(void){
     System_Clock_Init();
     LED_Init();
     UART2_Init();
     init_timer();
     USART_Write(USART2, (uint8_t *)"\r\nWelcome to Proj6\r\n", 20);
-    
-    TIM2->CCR1 = pwm_vars[0]; // voltage is +5V
-    //TIM2->CCR1 = pwm_vars[3]; // voltage is 0V
-    //TIM2->CCR1 = pwm_vars[5]; // voltage is -5V
-    //TIM2->CCR2 = pwm_vars[3]; // other motor example CCR2
 
     while(1){ // Poll indefinitely to read output of purplebox
-        if (is_event()){
-            //action seen on pin from purple box
+        if (is_event()){  //action seen on pin - need to get piece together pins for full number (input_voltage variable)
+            /* pseudo range for resolving ccr
+            if (input_voltage < -5.0 || input_voltage > 5.0){
+                RED_LED_ON();
+                USART_Write(USART2, (uint8_t *)"\r\nVoltage out of range.\r\n", 25);
+            }
+            else{
+                RED_LED_OFF();
+                double fine_ccr = (-1.70*input_voltage)+12.727;
+                int rounded_ccr = round(fine_ccr);
+                TIM2->CCR1 = rounded_ccr;
+            }
+            */
         }
     }
 }
